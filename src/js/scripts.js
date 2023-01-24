caf48674cd1dbb5ee0eb3825739bfc853d5627ec
sra <sara.habib48@gmail.com>
2023-01-24 22:41:49 +0200

    mapping

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,72 +3,131 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
 //import {Box3, Vector3} from 'three';
+//import {planVertexShader} from 'three/examples/jsm/shaders/PlanVertexShader.js';
+//import {planFragmentShader} from './shaders/planFragmentShader.js';
+import {CSS2DRenderer, CSS2DObject} from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
-var action;
-var mixer = null;
-var clock = new THREE.Clock();
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
-      var renderer = new THREE.WebGLRenderer();
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      document.body.appendChild( renderer.domElement );
-            renderer.setClearColor(0x606060);
-            
-            var loader = new GLTFLoader();
-        loader.load( 'assets/scene.gltf', function ( gltf ) {
-                    
-                    var model = gltf.scene;
-                    scene.add(model);
-                    mixer = new THREE.AnimationMixer(model);
-                    action = mixer.clipAction(gltf.animations[0]);
-                    action.setLoop( THREE.LoopOnce );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-                    gltf.scene.position.set(0, -1, 0);
-                    },  
-                );
-            
-         document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+document.body.appendChild(renderer.domElement);
 
-            function onDocumentMouseDown( event ) 
-            {
-                if ( action !== null ) {
+const scene = new THREE.Scene();
 
-                    action.stop();
-                    action.play();
-                    
-                  }
-            }
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+);
+
+renderer.setClearColor(0xA3A3A3);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0,1,2);
+
+orbit.update();
+
+ const grid = new THREE.GridHelper(30, 30);
+ scene.add(grid);
+
+ const ambientLight = new THREE.AmbientLight(0xededed, 0.8);
+ scene.add(ambientLight);
+
+ const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+ scene.add(directionalLight);
+ directionalLight.position.set(10, 11, 7);
 
 
-            
 
-            //LIGHT
-            var ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-            scene.add(ambientLight);
-            light = new THREE.PointLight(0xffffff, 0.8, 18);
-            light.position.set(0,2,4);
-            light.castShadow = true;
-            light.shadow.camera.near = 0.1;
-            light.shadow.camera.far = 25;
-            scene.add(light);
+ const gltfLoader = new GLTFLoader();
 
-            camera.position.z = 5;
-            camera.position.set( 0, 0, 5 );
-            
-            var controls = new OrbitControls( camera, renderer.domElement );
-            controls.update();
+// const rgbeLoader = new RGBELoader();
 
-        var animate = function () {
-            requestAnimationFrame( animate );
+//renderer.outputEncoding = THREE.sRGBEncoding;
+// renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.toneMappingExposure = 4;
 
-            var delta = clock.getDelta();
-          
-            if (mixer !== null) {
-              mixer.update(delta);
-            };
-          
-            renderer.render( scene, camera );
-      };
+ let car;
+     gltfLoader.load('./assets/scene.gltf', function(gltf) {
+         const model = gltf.scene;
+        //  gltf.scene.scale.set(0.25,0.25,0.25);
+        //  gltf.scene.rotation.y = Math.PI/2;
+        //  gltf.scene.rotation.y = 5
+        //  gltf.scene.rotation.z = -8;
+         model.scale.set(10,10,10);
+         scene.add(model);
+         car = model;
+ });
 
-      animate();
+
+//making the board
+// for(let i = 0; i < 8; i++) {
+//   for(let j = 0; j < 8; j++) {
+//     const geometry = new THREE.BoxGeometry(1, 1, 1);
+//     const material = new THREE.MeshBasicMaterial({wireframe: true});
+//     const boardTile = new THREE.Mesh(geometry, material);
+//     //NOTE: POSITIONING THE BOARD
+//     boardTile.position.x = i-8/2 +0.5;
+//     boardTile.position.y =j-8/2+ 0.5;
+//     scene.add(boardTile);
+//   }
+
+// }
+
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.domElement.style.pointerEvents = 'none';
+document.body.appendChild(labelRenderer.domElement);
+
+
+const page = document.getElementById('container');
+const div = document.createElement('div');
+console.log(page);
+div.appendChild(page);
+const label = new CSS2DObject(page);
+scene.add(label);
+
+
+
+//rend=new THREE.WebGLRenderer({canvas:canvas,antialias:true,alpha:true,transparent:true,premultipliedAlpha:false});
+const CanvasTexture = new THREE.CanvasTexture(scene);
+//making the plane
+const planeGeometry = new THREE.PlaneGeometry(32,32,32,32);
+const planeMaterial = new THREE.ShaderMaterial({
+  side: THREE.DoubleSide,
+  uniforms: {
+    time: {type: "f", value: 1.0},
+    uTime:{type: "f", value: 1.0},
+    uTexture: {value: CanvasTexture.Texture},
+    },
+
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    
+
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.scale.set(1.5,1.5,1.5);
+scene.add(planeMesh);
+
+
+function animate(time) {
+  labelRenderer.render(scene, camera);
+    //  if(car)
+    //      car.rotation.y = - time / 3000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    labelRenderer.setSize(window.innerWidth, window.innerHeight);
+});
