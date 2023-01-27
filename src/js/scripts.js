527285c69c52a721d9ee4e72d61a71f4c76b9d17
sra <sara.habib48@gmail.com>
2023-01-27 17:33:59 +0200

    a word on computer screen

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,9 +6,29 @@ import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
 //import {planVertexShader} from 'three/examples/jsm/shaders/PlanVertexShader.js';
 //import {planFragmentShader} from './shaders/planFragmentShader.js';
 import {CSS2DRenderer, CSS2DObject} from 'three/examples/jsm/renderers/CSS2DRenderer.js';
+import {CSS3DRenderer, CSS3DObject} from 'three/examples/jsm/renderers/CSS3DRenderer.js';
 import stars from '../img/windows.jpg';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+
+const canvas = document.getElementById("canvasID");
+
+var bitmap = document.createElement('canvas');
+ var g = bitmap.getContext('2d');
+ bitmap.width = 100;
+ bitmap.height = 100;
+ g.font = 'Bold 20px Arial';
+
+ g.fillStyle = 'white';
+ g.fillText('text', 0, 20);
+ g.strokeStyle = 'black';
+ g.strokeText('text', 0, 20);
+//var bitmap = document.getElementsByTagName('html')[0].innerHTML;
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -22,6 +42,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 );
+
 
 renderer.setClearColor(0xA3A3A3);
 
@@ -41,62 +62,43 @@ orbit.update();
  scene.add(directionalLight);
  directionalLight.position.set(10, 11, 7);
 
-
+//  let labelRenderer = new CSS3DRenderer();
+//  labelRenderer.setSize(window.innerWidth, window.innerHeight);
+//  labelRenderer.domElement.style.position = 'absolute';
+//  labelRenderer.domElement.style.top = 0;
+//  labelRenderer.domElement.style.pointerEvents = 'none';
+//  document.getElementById('canvasID').appendChild(labelRenderer.domElement);
+ 
+//  let el = document.getElementById("canvasID");
+//  el.innerHTML="<h1>Hello world</h1>";
+//  let label = new CSS3DObject(el);
+//  scene.add(label);
 
  const gltfLoader = new GLTFLoader();
-
 // const rgbeLoader = new RGBELoader();
 
 //renderer.outputEncoding = THREE.sRGBEncoding;
 // renderer.toneMapping = THREE.ACESFilmicToneMapping;
 // renderer.toneMappingExposure = 4;
 
- let car;
-     gltfLoader.load('./assets/scene.gltf', function(gltf) {
-         const model = gltf.scene;
-        //  gltf.scene.scale.set(0.25,0.25,0.25);
-        //  gltf.scene.rotation.y = Math.PI/2;
-        //  gltf.scene.rotation.y = 5
-        //  gltf.scene.rotation.z = -8;
-         model.scale.set(10,10,10);
-         scene.add(model);
-         car = model;
- });
-
-
-//making the board
-// for(let i = 0; i < 8; i++) {
-//   for(let j = 0; j < 8; j++) {
-//     const geometry = new THREE.BoxGeometry(1, 1, 1);
-//     const material = new THREE.MeshBasicMaterial({wireframe: true});
-//     const boardTile = new THREE.Mesh(geometry, material);
-//     //NOTE: POSITIONING THE BOARD
-//     boardTile.position.x = i-8/2 +0.5;
-//     boardTile.position.y =j-8/2+ 0.5;
-//     scene.add(boardTile);
-//   }
-
-// }
-
-const labelRenderer = new CSS2DRenderer();
-labelRenderer.setSize(window.innerWidth, window.innerHeight);
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = '0px';
-labelRenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(labelRenderer.domElement);
-
-
-const page = document.getElementById('container');
-const div = document.createElement('div');
-console.log(page);
-div.appendChild(page);
-const label = new CSS2DObject(page);
-scene.add(label);
+  let car;
+      gltfLoader.load('./assets/scene.gltf', function(gltf) {
+          const model = gltf.scene;
+          model.scale.set(10,10,10);
+          scene.add(model);
+          car = model;
+  });
 
 
 
-//rend=new THREE.WebGLRenderer({canvas:canvas,antialias:true,alpha:true,transparent:true,premultipliedAlpha:false});
-const CanvasTexture = new THREE.CanvasTexture(scene);
+// Render target
+
+//const renderTarget = new THREE.CanvasTexture(canvas);
+//renderTarget.needsUpdate = true;
+
+
+
+
 //making the plane
 const planeGeometry = new THREE.PlaneGeometry(8,8,8,8);
 const planeMaterial = new THREE.ShaderMaterial({
@@ -104,14 +106,14 @@ const planeMaterial = new THREE.ShaderMaterial({
   uniforms: {
     time: {type: "f", value: 1.0},
     uTime:{type: "f", value: 1.0},
-    uTexture: {value: CanvasTexture.Texture},
-    image: {type: "t", value: new THREE.TextureLoader().load(stars)},
+    uTexture: {type: "t",value: new THREE.CanvasTexture(bitmap)},
+    //image: {type: "t", value: new THREE.TextureLoader().load(stars)},
     },
 
     vertexShader: document.getElementById('vertexShader').textContent,
     fragmentShader: document.getElementById('fragmentShader').textContent,
     // wireframe: false
-    
+
 
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -122,7 +124,8 @@ scene.add(planeMesh);
 
 
 function animate(time) {
-  labelRenderer.render(scene, camera);
+  //labelRenderer.render(scene, camera);
+  //label.rotation.y += 0.01;
     //  if(car)
     //      car.rotation.y = - time / 3000;
     renderer.render(scene, camera);
@@ -134,5 +137,5 @@ window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    labelRenderer.setSize(window.innerWidth, window.innerHeight);
+    //labelRenderer.setSize(window.innerWidth, window.innerHeight);
 });
